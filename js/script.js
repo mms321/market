13bb66bbb2420cd49972af77e4b4dec22dddc0f2
2mm4 <24a_mms@ztu.edu.ua>
2025-04-21 09:07:59 +0300

    перевіркa існування знайденого блоку

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 console.log('Перевірка підключеного файлу скриптів script.js')
+
+let itemsDiv = document.getElementById("items");
+
+if (itemsDiv) {
+    console.log(itemsDiv)
+} else {
+    console.log('Блок товарів не знайдено')
+}
