b2b352772863b034007278b8ec568f83b6b612d1
2mm4 <24a_mms@ztu.edu.ua>
2025-04-21 09:23:40 +0300

    100 блоків

--- js/script.js
+++ js/script.js
@@ -19,12 +19,3 @@ for (let i = 0; i < 100; i++) {
 } else {
     console.log('Блок товарів не знайдено')
 }
-// Додавання відформатованого HTML коду в блок
-// itemsDiv.innerHTML += '<div class = "item"></div>'
-// itemsDiv.innerHTML += '<div class = "item"></div>'
-// itemsDiv.innerHTML += '<div class = "item"></div>'
-// itemsDiv.innerHTML += '<div class = "item"></div>'
-
-for (let i = 0; i < 100; i++) {
-    itemsDiv.innerHTML += '<div class = "item"></div>'
-}
