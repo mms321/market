f9f2e2d81e82a8710b1f36a6bd83129305a88709
2mm4 <24a_mms@ztu.edu.ua>
2025-04-21 09:14:41 +0300

    Програмно можна змінювати значення цих полів.

--- js/script.js
+++ js/script.js
@@ -4,6 +4,13 @@ let itemsDiv = document.getElementById("items");
 
 if (itemsDiv) {
     console.log(itemsDiv)
+
+    console.log('Поле classlist: ', itemsDiv.classList)
+    console.log('Поле id: ', itemsDiv.id)
+    console.log('Поле clientWidth: ', itemsDiv.clientWidth)
+    console.log('Поле innerHTML: ', itemsDiv.innerHTML)
+
+    itemsDiv.innerText = 'Програмно доданий текст'
 } else {
     console.log('Блок товарів не знайдено')
 }
